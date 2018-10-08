a76bd5d7d968d958b0a1553d9d09a31ac8ac8cc2
GoldraK <goldrak@gmail.com>
2018-10-08 18:44:54 +0700

    Cambio carga de ficheros CSS y JS de la manera clásica al cargador de WP

--- functions.php
+++ functions.php
@@ -43,27 +43,63 @@ function add_widget_Support() {
 add_action( 'widgets_init', 'add_Widget_Support' );
 
 
+add_action( 'wp_enqueue_scripts', 'enqueue_load_styles' );
+
+function enqueue_load_styles() {
+
+    wp_enqueue_style( 'bootstrap4', esc_url( get_template_directory_uri() ).'/css/bootstrap.min.css');
+
+    wp_enqueue_style( 'gfont', 'https://fonts.googleapis.com/css?family=Playfair+Display:700,900');
+
+    wp_enqueue_style( 'style', esc_url( get_template_directory_uri() ).'/style.css');
+
+    wp_enqueue_style( 'flaticon', esc_url( get_template_directory_uri() ) . '/css/flaticon.css');
+
+    wp_register_style( 'baguettebox',esc_url( get_template_directory_uri() ).'/css/baguetteBox.min.css');
+
+}
+
+
+add_action( 'wp_enqueue_scripts', 'enqueue_load_scripts' );
+
+function enqueue_load_scripts() {
+
+    wp_enqueue_script('jquery3',esc_url( get_template_directory_uri() ).'/js/jquery-3.3.1.slim.min.js',array(),'3.3.1',true);
+
+    wp_enqueue_script('popper',esc_url( get_template_directory_uri() ).'/js/popper.min.js',array('jquery3'),'1.14.3',true);
+ 
+    wp_enqueue_script('bootstrap4',esc_url( get_template_directory_uri() ).'/js/bootstrap.min.js',array('jquery3','popper'),'4.1.3',true);
+
+    wp_register_script('baguettebox',esc_url( get_template_directory_uri() ).'/js/baguetteBox.min.js',array(),'1.11.0',true);
+
+    wp_register_script('baguettebox-js-start',get_template_directory_uri().'/js/baguetteBox_start.js',array('baguettebox'),'0.1',true);
+}
+
+
+
+
+
 function bootstrap_starter_category()
 {
   if (has_category()) : ?>
-    <p><?php 
-    echo _n(
+    <?php 
+     return _n(
       'Category',
       'Categories',
       count(get_the_category()),
       'bootstrap-starter'
-      );?> <?php echo get_the_category_list(', ') ?></p>
-  <?php endif;
+      ). get_the_category_list(', '); 
+   endif;
 
 }
 
 function bootstrap_starter_tags()
 {
-  echo get_the_tag_list('<p>'._n(
+  return get_the_tag_list(_n(
     'Tag',
     'Tags',
     !get_the_tags() ? 0 : count(get_the_tags()),
-    'bootstrap-starter').": ",', ','</p>');
+    'bootstrap-starter').": ",', ','');
 }
 
 function bootstrap_starter_title_thumbnail()
--- header.php
+++ header.php
@@ -6,12 +6,6 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-
-    <!-- Custom styles for this template -->
-    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:700,900" rel="stylesheet">
-    <link href="<?php echo esc_url( get_template_directory_uri() );?>/style.css" rel="stylesheet">
 
     <?php wp_head();?>
 
