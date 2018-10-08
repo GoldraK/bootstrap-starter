ccae7fb34acaab00bca0a55a704f7ba4580005db
GoldraK <goldrak@gmail.com>
2018-10-08 18:51:25 +0700

    Nuevo codigo para el Loop, sacando la carga general a un nuevo fichero

--- index.php
+++ index.php
@@ -6,30 +6,14 @@
 
     <div class="col-md-10">
 
-      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
-        <article class="blog-post">
-          <div class="row">
-            <?php 
-              if ( has_post_thumbnail() ) : ?>
-              <div class="col-md-3">
-              <?php 
-                the_post_thumbnail('medium_large',['class' => 'img-fluid', 'alt' => esc_html ( bootstrap_starter_title_thumbnail() ) ]); ?>
-            </div>
-          <?php endif; ?>
-            <div  <?php if ( has_post_thumbnail() ) : ?>class="col-md-9" <?php endif; ?>>
-              <h2 class="blog-post-title"><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-              
-              <p class="blog-post-meta"><?php echo get_the_date( 'Y-m-d' ); ?> <?php _e('by' , 'bootstrap-starter')?> <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ), get_the_author_meta( 'user_nicename' ) ); ?>"><?php the_author(); ?></a></p>
-              
-              <p><?php the_excerpt(); ?></p>
-            </div>
-          </div>
+      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); 
+          
+          get_template_part( 'template-parts/content', get_post_format() );
         
-        </article>  
-
-        <?php endwhile; 
-        get_template_part('parts/pagination');
+        endwhile; 
+        
+        get_template_part('template-parts/pagination');
+        
         else : ?>
 
           <article>
